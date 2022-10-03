5b99139c24a171a11046feb8abc755068402dc73
Alastair <ali.dee1410@gmail.com>
2022-10-04 00:22:15 +0100

    Added ICON emoji definition

--- extensions/core/emojis.py
+++ extensions/core/emojis.py
@@ -1,12 +1,13 @@
 from bot import AlisUnnamedBot
 
 # Constants for custom emojis
+ICON = "<:alis_unnamed_bot:1025815376036106422>"
 TICK = "<:green_tick:1019746883742216233>"
 WARNING = "<:yellow_warning:1019746887970062386>"
 CROSS = "<:red_cross:1019746886799855626>"
 LOADING = "<a:loading:1019746885768056862>"
 ARROW_RIGHT = "<a:right_arrow:1021393428841504768>"
-LEVEL = "<:alis_unnamed_bot:1025815376036106422>"
+LEVEL = ICON
 WALLET = "<:wallet:1022912717599801475>"
 BANK = ":bank:"
 MONEY_BAG = ":moneybag:"
